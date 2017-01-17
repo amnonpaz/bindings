3e36cfd108d160a9146f6b343220c260193398e3
paz <Amnon Paz>
2017-01-17 23:54:26 +0200

    Python test: Minor edit

--- tests/python/UnixSocket.py
+++ tests/python/UnixSocket.py
@@ -12,7 +12,6 @@ except Exception as e:
     print('Error opening socket {0}: "{1}"'.format(socketName, e))
     quit()
 
-
 words = [ 'Hi there', 'Just testing you from python', 'Third message', 'Another message, just before leaving', 'So long']
 
 for word in words:
@@ -24,5 +23,3 @@ for word in words:
     except IOError as e:
         print('Error communicating with listener: "{0}"'.format(e))
         break
-
-
